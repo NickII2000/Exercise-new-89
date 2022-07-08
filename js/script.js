13fcadb5fd8e4e0545052f42d38f0b2c7fd13a17
Nick <Nick_II2000@mail.ru>
2022-07-08 23:38:25 +0600

    9-th commit

--- js/script.js
+++ js/script.js
@@ -323,9 +323,9 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 3000);
     }
 
-    // fetch('https://jsonplaceholder.typicode.com/todos/1')
-    //     .then(response => response.json())
-    //     .then(json => console.log(json));
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json));
 
     // fetch('https:/jsonplaceholder.typicode.com/todos/7', { metod: 'GET' })
     //     .then(response => response.json())
