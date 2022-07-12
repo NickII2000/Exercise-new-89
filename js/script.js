1f993eb9f9cf9a2144bedf25bc2efc96d6fe539d
Nick <Nick_II2000@mail.ru>
2022-07-12 20:28:27 +0600

    19-th commit

--- js/script.js
+++ js/script.js
@@ -327,19 +327,19 @@ window.addEventListener('DOMContentLoaded', () => {
         .then(response => response.json())
         .then(json => console.log(json));
 
-    // fetch('https:/jsonplaceholder.typicode.com/todos/7', { metod: 'GET' })
-    //     .then(response => response.json())
-    //     .then(json => console.log(json));
+    fetch('https:/jsonplaceholder.typicode.com/todos/7', { metod: 'GET' })
+        .then(response => response.json())
+        .then(json => console.log(json));
 
-    // fetch('https:/jsonplaceholder.typicode.com/posts', {
-    //     method: "POST",
-    //     body: JSON.stringify({ name: 'Nick' }),
-    //     headers: {
-    //         'Content-type': 'application/json'
-    //     }
-    // })
-    //     .then(response => response.json())
-    //     .then(json => console.log(json));
+    fetch('https:/jsonplaceholder.typicode.com/posts', {
+        method: "POST",
+        body: JSON.stringify({ name: 'Nick' }),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(json => console.log(json));
 
     // json - server(Exercise 89)
 
